2ab0c326307dd7af16d1ff8e9314636cc123179f
Brutix <brutix80@gmail.com>
2025-07-10 02:06:27 +0300

    merging

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,6 @@
 export const environment = {
   production: false,
   availableCountriesUrl: 'https://date.nager.at/api/v3/AvailableCountries/',
-<<<<<<< HEAD
-  holidayApiUrl: 'https://date.nager.at/api/v3/PublicHolidays/'
-=======
   holidayApiUrl: 'https://date.nager.at/api/v3/PublicHolidays'
->>>>>>> 281c5619479dbcda78a96e0882c9cab897fd1552
 
 };
